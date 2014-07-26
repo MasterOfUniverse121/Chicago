b55ccd85ed1c1fc037c28b757b949156844ca89b
Your Name <you@example>
2014-07-26 00:14:05 +0000

    edits

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/show' => 'application#show'
   get '/correct' => 'application#correct'
   get '/wrong' => 'application#wrong'
+  get '/city' => 'application#city'
 end
