5df5e6616cf94c4ee9dcff94d9b9d553f291e420
Your Name <you@example>
2014-07-27 22:42:15 +0000

    IWINUSUCK

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 protect_from_forgery with: :exception 
+  
   def chicago
   end
+  
   def city
-    if params['city'].downcase == 'chicago'
+    if params['city'] == 'chicago'
       redirect_to '/good'
     else
       redirect_to '/bad'
