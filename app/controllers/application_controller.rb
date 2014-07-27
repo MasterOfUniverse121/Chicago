3c15a19fef160cf00911882572f38883209b1ae2
Your Name <you@example>
2014-07-27 21:20:20 +0000

    back

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception 
   def city
-    if params;'city' == 'Chicago'
+    if params['name'] == 'Chicago'
       redirect_to '/correct'
     else
       redirect_to '/wrong'
