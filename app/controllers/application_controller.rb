6b85624e2f1b5e18f6f8c35bed1dd548a4be41e2
Your Name <you@example>
2014-07-27 22:04:56 +0000

    bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception 
+protect_from_forgery with: :exception 
+  def chicago
+  end
   def city
-    if params['name'] == 'Chicago'
+    if params['city'] == 'Chicago'
       redirect_to '/correct'
     else
       redirect_to '/wrong'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/show' => 'application#show'
+  get '/chicago' => 'application#chicago'
   get '/correct' => 'application#correct'
   get '/wrong' => 'application#wrong'
   get '/city' => 'application#city'
